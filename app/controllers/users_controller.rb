84f748a841f8456ac8b53a71cd9e60d898cb036d
krzubair <krzubairkhan@gmail.com>
2023-05-30 11:45:19 +0530

    edit user functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show]
+    before_action :set_user, only: [:show, :edit, :update]
     
     def index
         @user = User.all
@@ -23,6 +23,19 @@ class UsersController < ApplicationController
 
     def show
         # @user = User.find(params[:id])
+    end
+
+    def edit
+
+    end
+
+    def update 
+        if @user.update(user_params)
+            flash[:success] = "User updated Successfully"
+            redirect_to user_path(@user)
+        else    
+            render :edit
+        end
     end
       
     private
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources  :users
+resources  :users
 
  get 'signin', to: 'sessions#new'
  post 'signin', to: 'sessions#create'
